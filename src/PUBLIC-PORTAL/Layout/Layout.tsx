917f85a17d03b25b0661fd2a7b08926a9a78d28b
keerthanahestia <keerthana@hestiatechnology.com>
2026-09-09 10:38:46 +0530

    layout

--- src/PUBLIC-PORTAL/Layout/Layout.tsx
+++ src/PUBLIC-PORTAL/Layout/Layout.tsx
@@ -7,18 +7,12 @@ import Footer from "./Footer";
 const PublicLayout: React.FC = () => {
   return (
     <div className="d-flex flex-column min-vh-100">
-
-      {/* Fixed Navbar */}
       <div className="position-sticky top-0 z-3">
         <PublicNavbar />
       </div>
-
-      {/* Page Content */}
       <Container fluid className="flex-grow-1 px-0">
         <Outlet />
       </Container>
-
-      {/* Footer */}
       <Footer />
     </div>
   );
